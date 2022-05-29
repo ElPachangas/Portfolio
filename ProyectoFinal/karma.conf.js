b10d7aad23368ad73d37dcede49c4d938503aa34
Gaston Elias <96868564+ElPachangas@users.noreply.github.com>
2022-05-29 08:30:17 -0300

    y AHORA?

--- ProyectoFinal/karma.conf.js
+++ ProyectoFinal/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/proyecto-gaston'),
+      dir: require('path').join(__dirname, './coverage/proyecto-final'),
       subdir: '.',
       reporters: [
         { type: 'html' },
